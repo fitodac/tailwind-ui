aece0af182642f71f85f7f860894662975fc29b5
fitodac <fitodac@gmail.com>
2023-05-20 05:37:39 -0300

    extra page

---
+++ src/pages/extra.jsx
@@ -0,0 +1,16 @@
+import Layout from '../layouts/Layout'
+import PageHeader from '../components/PageHeader'
+
+export default function PageExtra(){
+	return (
+		<Layout>
+			<PageHeader title="Extra classes" />
+
+			<ul>
+				<li>
+					<code>.text-xxs</code> ( font-size: 9px;)
+				</li>
+			</ul>
+		</Layout>
+	)
+}
