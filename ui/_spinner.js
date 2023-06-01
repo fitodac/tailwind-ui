8773e668d44a767eb5c373cdd96ca8c5d5162219
fitodac <fitodac@gmail.com>
2023-06-01 10:02:30 -0300

    spinner

---
+++ ui/_spinner.js
@@ -0,0 +1,48 @@
+const theme = require('tailwindcss/defaultTheme')
+const GLOBALS = require('./_utils')
+
+
+const SPINNER_STYLE_BASE = {
+	DEFAULT: {
+		'font-weight': theme.fontWeight.md,
+		'line-height': theme.fontSize.xs[1],
+		'width': theme.spacing[10],
+		'display': 'inline-grid',
+		'place-content': 'center',
+		'position': 'relative',
+		'aspect-ratio': theme.aspectRatio.square
+	},
+
+	AFTER: {
+		'content': '""',
+		'border-left-color': 'inherit !important',
+		'border-width': theme.borderWidth[4],
+		'width': '100%',
+		'position': 'absolute',
+		'aspect-ratio': theme.aspectRatio.square,
+		'user-select': 'none',
+		'border-radius': theme.borderRadius.full,
+		'animation': theme.animation.spin
+	}
+}
+
+const SPINNER_UTILITIES = {
+	'.spinner': {
+		...SPINNER_STYLE_BASE.DEFAULT,
+
+		'&::after': {
+			...SPINNER_STYLE_BASE.AFTER
+		},
+
+		'&.w-3::after, &.w-4::after, &.w-5::after, &.w-6::after': {
+			'border-width': theme.borderWidth[2]
+		},
+
+		'&:not([class*=border-l-])': {},
+	}
+}
+
+
+// `@keyframes spin { from{ transform: rotate(0); } to{ ${theme.keyframes.spin.to} } }`: null
+
+module.exports = { SPINNER_UTILITIES }
