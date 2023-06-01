c28dfefe0ada8c1e98c3dfe790d70019e817c314
fitodac <fitodac@gmail.com>
2023-06-01 10:02:12 -0300

    Badge

--- ui/_badge.js
+++ ui/_badge.js
@@ -3,10 +3,32 @@ const GLOBALS = require('./_utils')
 
 
 const BADGE_STYLE_BASE = {
-	
+	DEFAULT: {
+		'border-width': theme.borderWidth.DEFAULT,
+		'font-size': theme.fontSize.xs[0],
+		'font-weight': theme.fontWeight.md,
+		'line-height': theme.fontSize.xs[1],
+		'padding': `${theme.spacing[0.5]} ${theme.spacing[2]} ${theme.spacing[1]}`,
+		'border-radius': theme.borderRadius.DEFAULT
+	},
+
+	DOT: {
+		'color': 'transparent',
+		'font-size': 0,
+		'width': theme.spacing[3],
+		'height': theme.spacing[3],
+		'right': `-${theme.spacing[0.5]}`,
+		'top': `-${theme.spacing[0.5]}`,
+		'position': 'absolute',
+		'overflow': 'hidden',
+		'border-radius': theme.borderRadius.full
+	}
 }
 
-const BADGE_UTILITIES = {}
+const BADGE_UTILITIES = {
+	'.badge': { ...BADGE_STYLE_BASE.DEFAULT },
+	'.dot-badge': { ...BADGE_STYLE_BASE.DOT }
+}
 
 
 module.exports = { BADGE_UTILITIES }
