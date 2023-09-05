d247456cbda7abf3c17476a471e6ccb91c3baad3
fitodac <fitodac@gmail.com>
2023-09-04 22:36:14 -0300

    Changes in demoCard for code toggler

--- src/components/DemoCard.jsx
+++ src/components/DemoCard.jsx
@@ -1,4 +1,14 @@
+import  { useState, useEffect } from 'react'
+
+import CodeBox from './CodeBox'
+
+
 export default function DemoCard(props){
+
+	const [ codeVisible, setCodeVisible ] = useState(false)
+
+	useEffect(() => {console.log('props:', props)}, [])
+	
 	return (
 		<div 
 			className="bg-white border border-slate-100 max-w-5xl mx-auto rounded-xl shadow
@@ -8,31 +18,27 @@ export default function DemoCard(props){
 				<div className="text-slate-600 text-sm font-semibold dark:text-slate-300">{ props.title }</div>
 
 				<nav>
-					<button className="btn btn-icon btn-outline hover:bg-slate-200 hover:border-slate-200">
-						<i className="ri-code-s-slash-line text-slate-400"></i>
-					</button>
+					{ props.code ? 
+					
+							!codeVisible
+								? (<button 
+										className="btn btn-icon btn-outline hover:bg-slate-200 hover:border-slate-200"
+										onClick={ () => setCodeVisible(true) }>
+										<i className="ri-code-s-slash-line text-slate-400"></i>
+									</button>)
+								: (<button 
+										className="btn btn-icon btn-outline hover:bg-slate-200 hover:border-slate-200"
+										onClick={ () => setCodeVisible(false) }>
+										<i className="ri-code-line text-slate-400"></i>
+									</button>) 
+
+						: null }
 				</nav>
 			</div>
 
+			<div className="px-6 pt-6 pb-8">{ props.children }</div>
 
-			<div className="px-6 pt-6 pb-8">
-				{ props.children }
-			</div>
-
-			<div className="bg-slate-700 pr-16 mx-6 mb-8 relative rounded-lg">
-				<button className="btn btn-icon btn-outline right-2 top-2 absolute hover:bg-slate-200 hover:border-slate-200">
-					<i className="ri-clipboard-line text-slate-400"></i>
-				</button>
-				
-				<pre className="text-slate-500 text-sm leading-tight p-6 overflow-x-auto">
-					<code className="text-white">{`<`}</code>
-					<code className="text-red-400">{`div`}</code>
-					<code className="text-orange-400"> {`class`}</code>
-					<code className="text-blue-400">{`=`}</code>
-					<code className="text-green-500">{`"bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500 bg-red-500"`}</code>
-					<code className="text-white">{`/>`}</code>
-				</pre>
-			</div>
+			{ codeVisible ? <CodeBox code={ props.code } /> : null }
 
 		</div>
 	)
